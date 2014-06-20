d1a13fdb093e74032786647d56a841e1d4a4fe22
efegraus <efegraus@conservation.org>
2014-06-20 14:21:32 -0700

    Added advanced camera trapping query and updated climate query

--- tools/team_db_query.R
+++ tools/team_db_query.R
@@ -1,6 +1,5 @@
 # team_db_query.r
 #
-#Hello!
 f.teamdb.query <- function(dataset) {
   # Database query function of TEAM production database. Returns a R Object for
   # each dataset and saves to the workspace. The function contains views or pre-
@@ -9,18 +8,20 @@ f.teamdb.query <- function(dataset) {
   # the R object. This will minimize hits on the database. 
   # Example usage: ctdata <- f.teamdb.query("camera trap")
   # Args:
-    # Dataset: The dataset you want. Acceptable args: "vegetation", "climate",
-    # "camera trap"
+  # Dataset: The dataset you want. Acceptable args: "vegetation", "climate",
+  # "camera trap", "camera trap adv". 
   # Returns:
-    # A dataframe or list with the desired datasets. 
-      # Camera trap dataset: cam_trap_data R object and dataframe
-      # Climate dataset: Returns a list with two dataframes in it.
-        # cl_data: All climate 2.0 and 3.0 data
-        # cl_temp_maxmin: The temperature maxmin associated with each TEAM Site.
-        #   This is used for quality control 
-      # Vegetation dataset: Returns a list wtih two dataframes in it:
-        # tree: The entire tree dataset
-        # liana: The entire liana dataset
+  # A dataframe or list with the desired datasets. 
+  # Camera trap datasets: cam_trap_data and cam_trap_data_adv R object and dataframe. 
+  # Camera trap adv will include much more camera trap metadata and image EXIF 
+  # information.
+  # Climate dataset: Returns a list with two dataframes in it.
+  # cl_data: All climate 2.0 and 3.0 data
+  # cl_temp_maxmin: The temperature maxmin associated with each TEAM Site.
+  #   This is used for quality control 
+  # Vegetation dataset: Returns a list wtih two dataframes in it:
+  # tree: The entire tree dataset
+  # liana: The entire liana dataset
   
   library(RPostgreSQL) # Need to explore RJDBC to connect with vertica
   drv <- dbDriver("PostgreSQL")
@@ -28,7 +29,17 @@ f.teamdb.query <- function(dataset) {
                    dbname="team_2.0_production",port="5444",
                    host="data.team.sdsc.edu")
   
-  if (dataset == "camera trap") {
+  if (dataset == "camera trap adv") {
+    cam_trap_query <- dbSendQuery(con,"select * from dqa_ct_advanced")
+    cam_trap_data_adv <- fetch(cam_trap_query, n = -1)
+    # Setup the filename with the Date. 
+    sysdate = Sys.Date()
+    filename= paste("ct_data_adv",sysdate,".gzip",sep="")
+    save(cam_trap_data_adv, file=filename,compress="gzip")
+    return(cam_trap_data_adv) # Return dataframe with CT data
+    
+    
+  } else if (dataset == "camera trap") {
     cam_trap_query <- dbSendQuery(con,"select * from dqa_ct_ordered")
     cam_trap_data <- fetch(cam_trap_query, n = -1)
     # Setup the filename with the Date. 
@@ -37,32 +48,70 @@ f.teamdb.query <- function(dataset) {
     save(cam_trap_data, file=filename,compress="gzip")
     return(cam_trap_data) # Return dataframe with CT data
     
-    
   } else if (dataset == "climate") {
-    cl_3_data <- dbSendQuery(con,"select collection_sampling_units.id, collection_sampling_units.sampling_unit_id, collections.site_id, cl_datalog_record.id, cl_datalog_record.collection_sampling_unit_id, cl_datalog_record.collected_at::timestamp without time zone, cl_datalog_record.batt_volt_min, cl_datalog_record.airtc_avg, cl_datalog_record.airtc_2_avg, cl_datalog_record.rain_mm_tot,cl_datalog_record.temp1_keep,cl_datalog_record.temp2_keep, cl_datalog_record.temp_clean,cl_datalog_record.precip_clean,cl_datalog_record.climate_review from collection_sampling_units,collections, cl_datalog_record where collection_sampling_units.collection_id = collections.id and collection_sampling_units.id = cl_datalog_record.collection_sampling_unit_id")
+    cl_3_data <- dbSendQuery(con,"SELECT * FROM dqa_climate3")
     data_3 <- fetch(cl_3_data, n = -1)
-    data_3["protocol"] <- 3 # Mark records as climate protocol 3
+    #data_3["protocol"] <- 3 # Mark records as climate protocol 3
     
     #** Get Clamate 2.0 data from climate_samples table 
-    cl_2_data <- dbSendQuery(con,"select collection_sampling_units.id, collection_sampling_units.sampling_unit_id, collections.site_id, climate_samples.id, climate_samples.collection_sampling_unit_id, climate_samples.collected_at, climate_samples.collected_time, climate_samples.dry_temperature as airtc_avg, climate_samples.precipitation as rain_mm_tot, climate_samples.temp1_keep,climate_samples.temp2_keep, climate_samples.temp_clean, climate_samples.precip_clean, climate_samples.climate_review from collection_sampling_units, collections, climate_samples where collection_sampling_units.collection_id = collections.id and collection_sampling_units.id = climate_samples.collection_sampling_unit_id")
+    cl_2_data <- dbSendQuery(con,"SELECT * FROM dqa_climate2_analysis")
     data_2 <- fetch(cl_2_data, n = -1)
-    data_2["protocol"] <- 2 # Mark records as climate protocol 2
-    data_2["airtc_2_avg"]<- NA # Add a new column to match what is in climate 3 data
+    # Get data_2 to match the structure of data_3
+    # Create new columns
     data_2$collected_at <- paste(data_2$collected_at,data_2$collected_time)
-    colnames(data_2)[7]<- "batt_volt_min"
-    data_2$batt_volt_min <- 0 # There are no battery values so climate 2.0 so make 0 and make column type = 'num'
-    data_2_new <-cbind(data_2[1:8],data_2[16],data_2[9:15]) # Rearrange columns so two datasets can be easily combined. 
-    
+    data_2$Observation <-data_2$collected_at
+    setnames(data_2,"Id","ReferenceID")
+    setnames(data_2,"TotalSolarRadiation","Sensor1_TotalSolarRadiation")
+    setnames(data_2,"Precipitation","Rainfall")
+    setnames(data_2,"DryTemperature","Sensor1_AvgTemp")
+    setnames(data_2,"RelativeHumidity","Sensor1_RelativeHumidity")
+    setnames(data_2,"SiteName","TEAMSiteName")
+    data_2["RecordID"]<- NA
+    data_2["MinimumBatteryVoltage"]<- NA
+    data_2["Sensor1_TempStdDeviation"]<- NA
+    data_2["Sensor2_AvgTemp"]<- NA
+    data_2["Sensor2_TempStdDeviation"]<- NA
+    data_2["Sensor2_RelativeHumidity"]<- NA
+    data_2["Sensor3_AvgTemp"]<- NA
+    data_2["Sensor3_TempStdDeviation"]<- NA
+    data_2["Sensor3_RelativeHumidity"]<- NA
+    data_2["Sensor1_AvgSolarRadiation"]<- NA
+    data_2["Sensor1_SolarRadiationStdDeviation"]<- NA
+    data_2["Sensor2_AvgSolarRadiation"]<- NA
+    data_2["Sensor2_SolarRadiationStdDeviation"]<- NA
+    data_2["Sensor2_TotalSolarRadiation"]<- NA
+    data_2["SerialNumber"]<- NA
+    data_2["ProgramName"]<- NA
+    data_2["OperatingSystem"]<- NA
+    data_2["Tachometer_RPM"]<- NA
+    # Get rid of these two coolumns
+    #data_2$ObservationDate <- NULL
+    #data_2$ObservationTime <- NULL
+    data_2_new <-data.frame(cbind(data_2$ReferenceID, data_2$Observation, data_2$RecordID, data_2$MinimumBatteryVoltage,
+                                  data_2$Sensor1_AvgTemp, data_2$Sensor1_TempStdDeviation, data_2$Sensor1_RelativeHumidity,
+                                  data_2$Sensor2_AvgTemp, data_2$Sensor2_TempStdDeviation, data_2$Sensor2_RelativeHumidity,
+                                  data_2$Sensor3_AvgTemp, data_2$Sensor3_TempStdDeviation, data_2$Sensor3_RelativeHumidity,
+                                  data_2$Sensor1_AvgSolarRadiation, data_2$Sensor1_SolarRadiationStdDeviation,data_2$Sensor1_TotalSolarRadiation,
+                                  data_2$Sensor2_AvgSolarRadiation, data_2$Sensor2_SolarRadiationStdDeviation,data_2$Sensor2_TotalSolarRadiation,
+                                  data_2$Rainfall,data_2$SerialNumber,data_2$ProgramName,data_2$OperatingSystem,
+                                  data_2$Tachometer_RPM, data_2$ProtocolVersion, data_2$SamplingUnitName, data_2$Latitude,
+                                  data_2$Longitude, data_2$TEAMSiteName))
+    colnames(data_2_new)<-c('ReferenceID','Observation','RecordID','MinimumBatteryVoltage',
+                            'Sensor1_AvgTemp','Sensor1_TempStdDeviation','Sensor1_RelativeHumidity',
+                            'Sensor2_AvgTemp','Sensor2_TempStdDeviation','Sensor2_RelativeHumidity',
+                            'Sensor3_AvgTemp','Sensor3_TempStdDeviation','Sensor3_RelativeHumidity',
+                            'Sensor1_AvgSolarRadiation','Sensor1_SolarRadiationStdDeviation','Sensor1_TotalSolarRadiation',
+                            'Sensor2_AvgSolarRadiation','Sensor2_SolarRadiationStdDeviation','Sensor2_TotalSolarRadiation',
+                            'Rainfall','SerialNumber','ProgramName','OperatingSystem',
+                            'Tachometer_RPM','ProtocolVersion','SamplingUnitName','Latitude',
+                            'Longitude','TEAMSiteName')
+    ####
+    data_2_new$Observation <-ymd_hms(data_2_new$Observation)
     # Combine climate 2.0 and 3.0 dataasets 
     cl_data_all = rbind(data_3,data_2_new)
-    
-    #  SQL to query from sites_climate_rules
-    cl_rules <- dbSendQuery(con,"select * from sites_climate_rules")
-    cl_temp_maxmin <- fetch(cl_rules, n = -1)
-    result <- list(cl_data = cl_data_all, cl_temp_maxmin = cl_temp_maxmin)
-    # Setup the filename with the Date. 
-    sysdate = Sys.Date()
-    filename= paste("cl_data",sysdate,".gzip",sep="")
+    result <- cl_data_all
+    sysdate <- Sys.Date()
+    filename <- paste("cl_data_new",sysdate,".gzip",sep="")
     save(result, file=filename,compress="gzip")
     return(result)
     
